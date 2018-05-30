d9ae9bb03754f0839538802836465f9016834950
André Claudino <andre.claudino@b2wdigital.com>
2018-05-30 16:30:57 -0300

    Melhora o Client de Services do kubernetes

---
+++ src/main/scala/com/b2w/kubernetes/cannary/kube_integration/ServicesClient.scala
@@ -0,0 +1,30 @@
+package com.b2w.kubernetes.cannary.kube_integration
+
+import io.kubernetes.client.ApiClient
+import io.kubernetes.client.Configuration
+import io.kubernetes.client.apis.CoreV1Api
+import io.kubernetes.client.util.Config
+
+class ServicesClient(namespace_name: String) {
+
+  val client:ApiClient = Config.defaultClient()
+  Configuration.setDefaultApiClient(client)
+
+  val api:CoreV1Api = new CoreV1Api()
+  api.setApiClient(api.getApiClient.setVerifyingSsl(false))
+
+  val destination_port_stable:Int = System.getenv("DESTINATION_PORT_STABLE").toInt
+  val destination_port_cannary:Int = System.getenv("DESTINATION_PORT_CANNARY").toInt
+
+  val destination_host_stable:String = System.getenv("DESTINATION_HOST_STABLE")
+  val destination_host_cannary:String = System.getenv("DESTINATION_HOST_CANNARY")
+
+  def destinationPortStable():Int = destination_port_stable
+
+  def destinationPortCannary():Int = destination_port_cannary
+
+  def destinationHostStable(stableSelector:String):String = destination_host_stable
+
+  def destinationHostCannary():String = destination_host_cannary
+
+}
