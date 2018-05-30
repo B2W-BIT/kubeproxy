8ef427cc2139dcf3a3d7a45de169a625e2ccbffd
André Claudino <andre.claudino@b2wdigital.com>
2018-05-30 16:30:20 -0300

    Adiciona a regra de decisão

---
+++ src/main/scala/com/b2w/kubernetes/cannary/rules/DecisionRule.scala
@@ -0,0 +1,23 @@
+package com.b2w.kubernetes.cannary.rules
+
+class DecisionRule() {
+  var decision: (Map[String, String], Map[String,String]) => String = (headers:Map[String, String], query:Map[String, String]) => "stable"
+
+  def loadDecision():Unit = {
+    // Simula o carregamento de uma função, num script ou arquivo por exemplo
+    this.decision = (_:Map[String, String], query:Map[String, String]) => query("version")
+  }
+
+  def decide(headers: Map[String, String], query: Map[String, String]):String = {
+    loadDecision()
+    try{
+      if(headers.isEmpty || query.isEmpty )
+        return "empty"
+      else
+        decision(headers, query)
+    }
+    catch{
+      case _: Throwable => "fail"
+    }
+  }
+}
